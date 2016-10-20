c2ddc4c8a169d7a85d2fc6b86e67fd54adf21bee
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-10-20 23:15:37 +0900

    make dir and file

---
+++ Hello-World/hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def hello_world():
+    """この関数は、hello world を出力する。
+
+    hello_world() で関数を呼び出す。
+    スクリプトとしては、$ python3 hello_world.py で出力する。
+    """
+
+    print("hello world")
+
+if __name__ == '__main__':
+    hello_world()
